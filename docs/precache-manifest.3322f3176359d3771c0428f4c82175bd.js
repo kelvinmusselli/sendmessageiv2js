c991f1a7692c39e4e9a9ab575cd24bd623ef929a
Jessiley Willian Valerio de Oliveira <xjaco222x@hotmail.com>
2019-06-21 16:04:55 -0300

    docs

---
+++ docs/precache-manifest.3322f3176359d3771c0428f4c82175bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f2e0d8a50f7b31fa7e0fa7e19a4e9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dc6a896ec97c475b76",
+    "url": "/static/css/main.8f11db49.chunk.css"
+  },
+  {
+    "revision": "d78bb45a5e5ca99c5805",
+    "url": "/static/js/2.b22c3b9f.chunk.js"
+  },
+  {
+    "revision": "65dc6a896ec97c475b76",
+    "url": "/static/js/main.2364d737.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
