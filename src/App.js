49514c7c85900b840b1a618178b2a08c2db534f5
Jessiley Willian Valerio de Oliveira <xjaco222x@hotmail.com>
2019-06-21 16:01:46 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import InputMask from 'react-input-mask';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [company, setComapny] = useState("");
+  const [giveway, setGiveway] = useState(1);
+
+  const [listItens, setListItens] = useState([
+    {name: "Jessiley 1", phone: "(11) 9 4767:3811 ", company: "TOTVS", giveway: 0}, 
+    {name: "Jessiley 2", phone: "(11) 9 4767:3822 ", company: "Vipal", giveway: 1}, 
+    {name: "Jessiley 3", phone: "(11) 9 4767:3833 ", company: "Grupo IV2", giveway: 1}, 
+    {name: "Jessiley 4", phone: "(11) 9 4767:3844 ", company: "Outro", giveway: 1}]);
+  useEffect(() => {
+    const componentDidMount = async () => {
+      setTimeout(() => {
+        setListItens([
+          {name: "Jessiley 1", phone: "(11) 9 4767:3811 ", company: "TOTVS", giveway: 1}, 
+          {name: "Jessiley 2", phone: "(11) 9 4767:3822 ", company: "Vipal", giveway: 0}, 
+          {name: "Jessiley 3", phone: "(11) 9 4767:3833 ", company: "Grupo IV2", giveway: 1}, 
+          {name: "Jessiley 5", phone: "(11) 9 4767:3844 ", company: "Outro", giveway: 1}]);
+      }, 2000);
+    }
+    
+    componentDidMount();
+  }, []);
+
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    let texto = "Texto que eu vou enviar \n com quebras de \n texto.";
+    texto = window.encodeURIComponent(texto);
+
+    const newItem = {name: name, phone: phone, comapny: company, giveway: giveway};
+    Axios.get(`https://api.whatsapp.com/send?phone=55${phone}&text=${texto}`)
+    console.log(newItem);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={_handleSubmit}>
+          <div><label>Nome: </label><input value={name} type="text" id="name" name="name" onChange={e => setName(e.target.value)}/></div>
+          <div><label>Telefone: </label><InputMask mask="(99) 9 9999-9999"  id="phone" name="phone" onChange={e => setPhone(e.target.value)} /></div>
+          <div><label>Nome: </label><input value={company} type="text" id="company" name="company" onChange={e => setComapny(e.target.value)}/></div>
+          <div>
+            <label><input value="1" type="radio" name="giveway" defaultChecked="true" onClick={e => setGiveway(1)} />Sim</label>
+            <label><input value="0" type="radio" name="giveway" onClick={e => setGiveway(0)}  />Não</label>
+          </div>
+          <div><input type="submit" value="Salvar"/></div>
+        </form>
+        <div className="list">
+          {listItens.length > 0 && listItens.map((item, key) => {
+            return(<div key={key}>
+              <div>{item.name}</div>
+              <div>{item.phone}</div>
+              <div>{item.comapny}</div>
+              <div>{item.giveway === 1 ? "Sim" : item.giveway === 0 ? "Não" : "erro"}</div>
+            </div>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
