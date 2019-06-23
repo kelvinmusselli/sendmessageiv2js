b79bd05ef91d208553776cd5b13720ee121165fc
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-23 14:46:42 -0300

    Updates

--- precache-manifest.e457853ed95dfec2370f8b7e666f0fab.js
+++ precache-manifest.e457853ed95dfec2370f8b7e666f0fab.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a034d0060b8886e320ce07be07b037f1",
+    "revision": "5f948ea5cce0713acc82dffd6b2fca7c",
     "url": "/sendmessageiv2js/index.html"
   },
   {
-    "revision": "f259b4b35b255969d00a",
+    "revision": "76b63a22dd6c114ecce1",
     "url": "/sendmessageiv2js/static/css/main.5ff81799.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sendmessageiv2js/static/js/2.b0b6164e.chunk.js"
   },
   {
-    "revision": "f259b4b35b255969d00a",
-    "url": "/sendmessageiv2js/static/js/main.43ad65df.chunk.js"
+    "revision": "76b63a22dd6c114ecce1",
+    "url": "/sendmessageiv2js/static/js/main.c9e6ccc8.chunk.js"
   },
   {
     "revision": "c0a7690ae82a8451b955",
