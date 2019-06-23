83a7f2093c0a699aeecc5a6eb324768bdfabf9d3
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-22 23:10:45 -0300

    Updates

---
+++ precache-manifest.c7d96b7ad256a52465ebee8b8dfa4be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416dba37118c17677aec4a861e24d97d",
+    "url": "/sendmessageiv2js/index.html"
+  },
+  {
+    "revision": "7382afaf2b44f235ba39",
+    "url": "/sendmessageiv2js/static/css/main.a64c58bd.chunk.css"
+  },
+  {
+    "revision": "b4481fe29f9f81e52c20",
+    "url": "/sendmessageiv2js/static/js/2.b0b6164e.chunk.js"
+  },
+  {
+    "revision": "7382afaf2b44f235ba39",
+    "url": "/sendmessageiv2js/static/js/main.54789fb5.chunk.js"
+  },
+  {
+    "revision": "c0a7690ae82a8451b955",
+    "url": "/sendmessageiv2js/static/js/runtime~main.fff6b61f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sendmessageiv2js/precache-manifest.c7d96b7ad256a52465ebee8b8dfa4be1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sendmessageiv2js/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
