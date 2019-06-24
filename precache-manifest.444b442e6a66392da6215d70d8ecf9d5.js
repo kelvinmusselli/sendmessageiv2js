a0902fae9d044b1f335c9a546d2b53d0180d3958
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-24 13:46:52 -0300

    Updates

--- precache-manifest.444b442e6a66392da6215d70d8ecf9d5.js
+++ precache-manifest.444b442e6a66392da6215d70d8ecf9d5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1592ff6b1b45c57f68269ca9902e0d85",
+    "revision": "00c5f46dc71d6a3cf99e0141131dd19f",
     "url": "/sendmessageiv2js/index.html"
   },
   {
-    "revision": "eca1b282cde0723457c6",
+    "revision": "b5284ee8dad8a4b7a27b",
     "url": "/sendmessageiv2js/static/css/main.7b89f0f5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sendmessageiv2js/static/js/2.59cedefb.chunk.js"
   },
   {
-    "revision": "eca1b282cde0723457c6",
-    "url": "/sendmessageiv2js/static/js/main.b6941da6.chunk.js"
+    "revision": "b5284ee8dad8a4b7a27b",
+    "url": "/sendmessageiv2js/static/js/main.0b87747b.chunk.js"
   },
   {
     "revision": "c0a7690ae82a8451b955",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sendmessageiv2js/precache-manifest.496622ff684d1f8c45e96c9c861ff156.js"
+  "/sendmessageiv2js/precache-manifest.444b442e6a66392da6215d70d8ecf9d5.js"
 );
 
 self.addEventListener('message', (event) => {
