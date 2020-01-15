3ef4c80180e0ff0141ff059ebf6e28243ba1e570
kelvin <kelvinmusselli22@gmail.com>
2020-01-15 08:24:18 -0300

    ok

--- src/components/NewMessageForm/NewContactForm.js
+++ src/components/NewMessageForm/NewContactForm.js
@@ -56,6 +56,7 @@ function NewContactForm(props) {
             ". Boa Sorte!\n\n"
           : "") +
         "E conforme conversamos, segue o material sobre os Apps e soluções da iv2.\n\n" +
+        "Conheça o Grupo iv2: https://drive.google.com/file/d/1X69rRSPQiiK3NJIYRGeuQtGcmXJYjooN/view?usp=sharing\n\n" +
         "Easy Quality https://www.youtube.com/watch?v=-xYYd9CbCo4\n\n" +
         // "Easy Flow https://www.youtube.com/watch?v=cPg9qQ6KTGc&list=PL9e5jlUEjJCmuXox8bNEcStXZwbZ2GebF\n" +
         "Make it Easy https://www.youtube.com/watch?v=b3a3gfTEtpo&t\n\n" +
